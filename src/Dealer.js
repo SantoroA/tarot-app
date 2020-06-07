4064b08f7f1d7a463a81568f3db014e08dac3561
SantoroA <64786843+SantoroA@users.noreply.github.com>
2020-06-07 19:01:21 -0300

    Started project
    Added tarot images
    Randomly select cards

---
+++ src/Dealer.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import TarotCard from "./TarotCard";
+
+export default function Dealer(props) {
+  const [face, setFace] = useState(10);
+  const cards = [
+    "The Fool",
+    "The Magician",
+    "High Priestess",
+    "The Empress",
+    "The Emperor",
+    "The Hierophant",
+    "The Chariot",
+  ];
+
+  function handleClick(e) {
+    e.preventDefault();
+    setFace(Math.floor(Math.random() * 7));
+  }
+  return (
+    <div>
+      <button onClick={handleClick}>Deal</button>
+      <TarotCard card={face} name={cards[face]} />
+    </div>
+  );
+}
---
+++ src/TarotCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./TarotCard.css";
+
+export default function TarotCard(props) {
+  function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => {
+      images[item.replace("./", "")] = r(item);
+    });
+    return images;
+  }
+
+  const images = importAll(
+    require.context("./steampunk-tarot", false, /\.(png|jpe?g|svg)$/)
+  );
+
+  return (
+    <div className="TarotCard">
+      <img src={images[`${props.card}.png`]} />
+      <p>{props.name}</p>
+    </div>
+  );
+}
